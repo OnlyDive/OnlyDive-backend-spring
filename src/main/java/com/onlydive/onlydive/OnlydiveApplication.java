30f47e8ebb065183ae9b2ba9426a9896614d6a91
dominikkorwek <dominikkorwek@interia.pl>
2024-08-06 19:04:52 +0200

    Update OnlydiveApplication.java

--- src/main/java/com/onlydive/onlydive/OnlydiveApplication.java
+++ src/main/java/com/onlydive/onlydive/OnlydiveApplication.java
@@ -1,12 +1,16 @@
 package com.onlydive.onlydive;
 
+import com.onlydive.onlydive.config.OpenApiConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(OpenApiConfig.class)
+
 public class OnlydiveApplication {
 
 	public static void main(String[] args) {
