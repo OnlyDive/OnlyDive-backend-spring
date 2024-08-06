4d4fc14c46b804f1c80673ee1597d8bd717131d1
dominikkorwek <dominikkorwek@interia.pl>
2024-08-06 19:04:48 +0200

    Create RefreshToken.java

---
+++ src/main/java/com/onlydive/onlydive/model/RefreshToken.java
@@ -0,0 +1,21 @@
+package com.onlydive.onlydive.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@RequiredArgsConstructor
+@Data
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private Instant createdDate;
+}
