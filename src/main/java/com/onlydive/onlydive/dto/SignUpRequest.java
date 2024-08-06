43f9b4e09c565c9a4e0ec6aac73b570cfa0ec12a
dominikkorwek <dominikkorwek@interia.pl>
2024-08-06 19:04:45 +0200

    Create SignUpRequest.java

---
+++ src/main/java/com/onlydive/onlydive/dto/SignUpRequest.java
@@ -0,0 +1,22 @@
+package com.onlydive.onlydive.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpRequest {
+    @Email
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "First Name is required")
+    private String firstName;
+    @NotBlank(message = "Last Name is required")
+    private String lastName;
+    @NotBlank(message = "Password is required")
+    private String password;
+}
