8ac92af04ac1cfdd50b5f5de59d39c926422dbb1
dominikkorwek <dominikkorwek@interia.pl>
2024-08-06 19:04:56 +0200

    Create AppUserDetailsService.java

---
+++ src/main/java/com/onlydive/onlydive/service/AppUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.onlydive.onlydive.service;
+
+import com.onlydive.onlydive.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+@RequiredArgsConstructor
+@Service
+@Scope("singleton")
+@Slf4j
+
+public class AppUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.onlydive.onlydive.model.User user =  userRepository.findByUsername(username).orElseThrow();
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new HashSet<>());
+    }
+}
