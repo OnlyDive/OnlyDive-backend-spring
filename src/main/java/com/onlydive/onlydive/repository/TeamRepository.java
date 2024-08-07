16560bbe00646d6b6bf75eda1cf688052a98b346
dominikkorwek <dominikkorwek@interia.pl>
2024-08-07 19:18:58 +0200

    Update TeamRepository.java

--- src/main/java/com/onlydive/onlydive/repository/TeamRepository.java
+++ src/main/java/com/onlydive/onlydive/repository/TeamRepository.java
@@ -2,6 +2,8 @@ package com.onlydive.onlydive.repository;
 
 import com.onlydive.onlydive.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
 }
