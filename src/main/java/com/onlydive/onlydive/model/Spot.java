0aac4d48a78a2b28c5b579775bed8862adf9bd57
dominikkorwek <dominikkorwek@interia.pl>
2024-08-07 19:18:05 +0200

    Create Spot.java

---
+++ src/main/java/com/onlydive/onlydive/model/Spot.java
@@ -0,0 +1,34 @@
+package com.onlydive.onlydive.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Spot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Double latitude;
+    @Column(nullable = false)
+    private Double longitude;
+    @Lob
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
+    private Set<SpotComment> comments;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private User creator;
+    private Instant creationDate;
+    //todo pogoda
+
+}
