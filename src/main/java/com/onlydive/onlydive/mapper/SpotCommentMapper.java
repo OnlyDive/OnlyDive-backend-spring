9aac8ba3a7ef9c57190c4a7ea570d4bc8be41ac7
dominikkorwek <dominikkorwek@interia.pl>
2024-08-08 08:40:44 +0200

    Create SpotCommentMapper.java

---
+++ src/main/java/com/onlydive/onlydive/mapper/SpotCommentMapper.java
@@ -0,0 +1,25 @@
+package com.onlydive.onlydive.mapper;
+
+import com.onlydive.onlydive.dto.SpotCommentRequest;
+import com.onlydive.onlydive.dto.SpotCommentResponse;
+import com.onlydive.onlydive.model.Spot;
+import com.onlydive.onlydive.model.SpotComment;
+import com.onlydive.onlydive.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public abstract class SpotCommentMapper {
+    protected final DataMapper dataMapper = new DataMapper();
+
+
+    @Mapping(target = "name",source = "request.name")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "creationDate", expression = "java(java.time.Instant.now())")
+    public abstract SpotComment mapToSpotComment(SpotCommentRequest request, Spot spot, User user);
+
+    @Mapping(target = "spotId", source = "spotComment.spot.id")
+    @Mapping(target = "username", source = "spotComment.user.username")
+    @Mapping(target = "creationDate", expression = "java(dataMapper.mapToString(spotComment.getCreationDate()))")
+    public abstract SpotCommentResponse mapToSpotCommentResponse(SpotComment spotComment);
+}
