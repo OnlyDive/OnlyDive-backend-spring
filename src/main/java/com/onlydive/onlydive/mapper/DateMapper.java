b26d8dd123fbc4da7dd8bd83e588f21b5b5fe3b6
dominikkorwek <dominikkorwek@interia.pl>
2024-08-08 23:19:25 +0200

    change data to date

--- src/main/java/com/onlydive/onlydive/mapper/DateMapper.java
+++ src/main/java/com/onlydive/onlydive/mapper/DateMapper.java
@@ -7,7 +7,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 @Mapper
-public class DataMapper {
+public class DateMapper {
 
     public String mapToString(Instant date){
         return DateTimeFormatter.ISO_LOCAL_DATE_TIME
