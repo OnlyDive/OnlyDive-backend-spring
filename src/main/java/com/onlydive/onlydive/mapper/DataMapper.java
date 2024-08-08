abd311e0cd3d60a52701f26481aa49ff72af03fe
dominikkorwek <dominikkorwek@interia.pl>
2024-08-08 08:40:41 +0200

    Create DataMapper.java

---
+++ src/main/java/com/onlydive/onlydive/mapper/DataMapper.java
@@ -0,0 +1,22 @@
+package com.onlydive.onlydive.mapper;
+
+import org.mapstruct.Mapper;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+@Mapper
+public class DataMapper {
+
+    public String mapToString(Instant date){
+        return DateTimeFormatter.ISO_LOCAL_DATE_TIME
+                .withZone(ZoneId.systemDefault())
+                .format(date);
+    }
+
+    public Instant mapToInstant(String date){
+        return Instant.parse(date);
+    }
+}
+
