87e0570d0ef2a80e5bc9d84c737b175be950d370
BBrejna <bartosz.brejna@gmail.com>
2024-08-13 23:49:28 +0200

    Added exception handling for validation errors in GlobalExceptionHandler

--- src/main/java/com/onlydive/onlydive/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/onlydive/onlydive/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,17 @@
 package com.onlydive.onlydive.exceptions;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.springframework.http.ResponseEntity.status;
 
@@ -25,6 +32,19 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e) {
         return status(HttpStatus.NOT_FOUND)
                 .body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream().map(FieldError::getDefaultMessage).toList();
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
     }
 //    @ExceptionHandler(RuntimeException.class)
 //    public ResponseEntity<Object> handleRuntimeException(RuntimeException e) {
